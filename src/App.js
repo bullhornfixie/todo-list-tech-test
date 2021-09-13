73a3f6e2f9d4bd982cf159dba6c0f03009429dcc
Jon Dawson <dawson.jon@hotmail.com>
2021-09-13 21:13:21 +0100

    initial draft

--- src/App.js
+++ src/App.js
@@ -1,56 +1,111 @@
+import React from "react";
+import { useState } from 'react'
 import { Flex, Text, Box } from 'rebass'
-import './styles/App.css';
 import { colors, font, sizes } from './styles/variables.js'
 import Header from './components/header'
 import styled from 'styled-components'
+import './styles/App.css';
 
-const InputBox = styled(Box)`
-  outline-style: solid;
-  width: 80%;
-  margin-top: ${sizes.md}px;
-  height: ${sizes.lg}px;
-`
-
-const tasks = ["wash car", "cook dinner", "do test"]
-
-const viewTasks = () => {
+const Todo = ({ todo, index, completeTodo, removeTodo }) => {
   return (
-    tasks.map((item) => (
-      <Flex 
-        width='80%' 
-        bg={colors.turquoise}
-        mt={sizes.md}
-        height={sizes.lg}
-        alignItems='center'
-      > 
-        <Text color={colors.black} pl={sizes.xs}>{item}</Text>
-      </Flex>
-    ))
-  )
+
+    <Flex flexDirection='row' justifyContent='space-between' mt='10px'
+      className="todo"
+      style={{ textDecoration: todo.isCompleted ? "line-through" : "" }}
+    >
+      {todo.text}
+       <Flex> 
+          <button onClick={() => completeTodo(index)}>Complete</button>
+          <button onClick={() => removeTodo(index)}>x</button>
+       </Flex>
+    </Flex>
+  );
+}
+
+const TodoForm = ({ addTodo }) => {
+  const [value, setValue] = useState("");
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (!value) return;
+    addTodo(value);
+    setValue("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="input"
+        value={value}
+        onChange={e => setValue(e.target.value)}
+      />
+    </form>
+  );
 }
 
 const App = () => {
+  const [todos, setTodos] = useState([
+    {
+      text: "Learn about React",
+      isCompleted: false
+    },
+    {
+      text: "Meet friend for lunch",
+      isCompleted: false
+    },
+    {
+      text: "Build really cool todo app",
+      isCompleted: false
+    }
+  ]);
+
+  const addTodo = text => {
+    const newTodos = [...todos, { text }];
+    setTodos(newTodos);
+  };
+
+  const completeTodo = index => {
+    const newTodos = [...todos];
+    newTodos[index].isCompleted = true;
+    setTodos(newTodos);
+  };
+
+  const removeTodo = index => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+
   return (
   <>
-   <Header />
-    <Flex 
-      flexDirection='column'
-      bg={colors.black}
-      height='100vh'
-      alignItems='center'
-      justifyContent='center'
-    >
-      <Flex
+    <Header />
+      <Flex 
         flexDirection='column'
+        bg={colors.black}
+        height='100vh'
         alignItems='center'
-        width='50vh'
-        height='50vh'
-        bg={colors.white}
+        justifyContent='center'
       >
-        <InputBox />
-        {viewTasks()}
+        <Flex
+          flexDirection='column'
+          width='50vh'
+          height='50vh'
+          padding='10px'
+          bg={colors.white}
+        >
+          {todos.map((todo, index) => (
+            <Todo
+              key={index}
+              index={index}
+              todo={todo}
+              completeTodo={completeTodo}
+              removeTodo={removeTodo}
+            />
+          ))}
+          <TodoForm addTodo={addTodo} />
+        </Flex>
       </Flex>
-    </Flex>
   </>
   );
 }
